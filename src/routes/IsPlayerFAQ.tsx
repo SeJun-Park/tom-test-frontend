dbeff94f49004b1325859b3eda46ec44badc9215
SeJun-Park <sejun9aldo@gmail.com>
2023-10-05 10:43:03 +0900

    FAQ

--- src/routes/IsPlayerFAQ.tsx
+++ src/routes/IsPlayerFAQ.tsx
@@ -1,3 +1,29 @@
+import { Button, HStack, Text, VStack } from "@chakra-ui/react";
+import { Helmet } from "react-helmet";
+import { FaArrowLeft } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+
 export default function IsPlayerFAQ() {
-    return <h1>P FAQ</h1>
+
+    const navigate = useNavigate();
+
+    const onClickBack = () => {
+        navigate(-1)
+    }
+
+    return (
+    <>
+        <Helmet>
+            <title> 삼오엠 | FAQ </title>
+        </Helmet>
+        <HStack height={20} px={5}>
+            <Button variant={"unstyled"} onClick={onClickBack}>
+                <FaArrowLeft />
+            </Button>
+        </HStack>
+        <VStack alignItems={"flex-start"} padding={"5"} mb={10}>
+            <Text fontSize={"xl"} as="b"> 준비중입니다 🥹 </Text>
+        </VStack>
+    </>
+    )
 }
