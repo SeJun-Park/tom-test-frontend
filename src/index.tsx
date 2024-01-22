85d5e024969887c05a32dda6f7d2fe4afd297b6d
SeJun-Park <sejun9aldo@gmail.com>
2024-01-22 14:03:53 +0900

    gtm -> index.html

--- src/index.tsx
+++ src/index.tsx
@@ -8,13 +8,6 @@ import router from './router';
 import theme from './theme';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { RecoilRoot } from "recoil";
-import TagManager from 'react-gtm-module';
-
-const tagManagerArgs = {
-  gtmId: 'GT-KDTS7T4'
-}
-
-TagManager.initialize(tagManagerArgs);
 
 const client = new QueryClient();
   // 여기서 React Query가 캐싱을 엄청나게 함
